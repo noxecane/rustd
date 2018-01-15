3bdf8332b7dd003c2abbc26e10474b243b20fb69
Arewa Olakunle <danceasarxx@gmail.com>
2018-01-15 09:15:09 +0100

    Closures(anonymous functions)

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,5 @@
-pub mod client;
-pub mod network;
 
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-    }
-
-    #[test]
-    fn break_loose() {
-        panic!("Call for help");
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,58 @@
 extern crate rustd;
 
+use std::thread;
+use std::time::Duration;
 
 fn main() {
-    println!("Fibonacci for 35 is {}", fib(35));
+    let fake_intensity = 24;
+    let fake_random_value = 8;
+
+    generate_workout(fake_intensity, fake_random_value);
 }
 
-fn fib(n: u32) -> u32 {
-    match n {
-        0 => 0,
-        1 => 1,
-        _ => fib(n - 1) + fib(n - 2)
+
+struct Memoizer<T: Fn(u32) -> u32> {
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Memoizer<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Memoizer<T> {
+        Memoizer {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
+fn generate_workout(intensity: u32, randomness: u32) {
+    let mut mem = Memoizer::new(|num| {
+        println!("In the toilet....");
+        thread::sleep(Duration::from_secs(3));
+        num
+    });
+    if intensity < 25 {
+        println!("Do {} pushups today!", mem.value(intensity));
+        println!("Do {} sitreps!", mem.value(intensity));
+    } else {
+        if randomness == 3 {
+            println!("Take a break. Don't forget to get hydrated");
+        } else {
+            println!("Run for {} mins!",mem.value(intensity));
+        }
     }
 }
